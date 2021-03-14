2d77706c41232f5b08fd11271b4589081e59fc72
Tanmay Chakraborty <75254780+tanmayChakrawarty@users.noreply.github.com>
2021-03-14 17:12:04 +0530

    added new APK

--- app/src/main/java/com/example/soc_macmini_15/sqlitepractice/Activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/soc_macmini_15/sqlitepractice/Activity/SplashScreenActivity.java
@@ -1,4 +1,26 @@
 package com.example.soc_macmini_15.sqlitepractice.Activity;
 
-public class SplashScreenActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.example.soc_macmini_15.sqlitepractice.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+//    ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreenActivity.this,RegisterActivity.class));
+                finish();
+            }
+        },5000);
+    }
 }
